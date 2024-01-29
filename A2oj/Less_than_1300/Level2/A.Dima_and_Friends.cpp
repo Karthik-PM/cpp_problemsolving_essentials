4a4af05ab3d2b1330e9fdf83d1b396906978a1eb
Karthik-PM <karthikpm52@gmail.com>
2024-01-29 16:45:11 +0530

    edits

--- A2oj/Less_than_1300/Level2/A.Dima_and_Friends.cpp
+++ A2oj/Less_than_1300/Level2/A.Dima_and_Friends.cpp
@@ -15,8 +15,8 @@ int main(int argc, char const *argv[])
 
     int number_of_ways = 0;
     for(int i = 1; i <= 5; i++) {
-        if(((total_fingers + i) % (n + 1)) != 1){
-            number_of_ways ++;
+        if(((total_fingers + i) % (n + 1)) != 1){ // this is like a cirular arragement where the last position is 0 ( since n % n = 0)
+            number_of_ways ++; // therefore if the result comes back to first position then no need to select that number
         } 
     }
     std::cout << number_of_ways << "\n";
