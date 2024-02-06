e93446763857cafdc4c7d2cc4a5123c7fd1160de
Karthik-PM <karthikpm52@gmail.com>
2024-02-06 19:04:42 +0530

    Problem 39

---
+++ A2oj/Less_than_1300/Level2/A.Hexadecimal_s_theorem.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[])
+{
+    int n; 
+    std::cin >> n;
+    std::cout << 0 << " " << 0 << " " << n; // zero is a fibonacci number and the number it self is also a fibonacci number
+    return 0;
+}
