9a562e5d480f49e37caa72d03757f868c4aa4aeb
Karthik-PM <karthikpm52@gmail.com>
2025-02-23 13:10:41 +0530

    added adjistancy list

---
+++ data_structures/graphs/adj_list.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <set>
+class GraphNode{
+private:
+    std::string val;
+    std::vector<std::string> neighbours;
+
+    GraphNode(std::string& val) : val(val){
+
+    }
+};
+
+std::unordered_map<std::string, std::vector<std::string>> buildAdjList(std::vector<std::vector<std::string>> edges){
+    std::unordered_map<std::string,std::vector<std::string>> Adjlist;
+    for(auto edge : edges){
+        std::string src = edge[0], des = edge[1];
+
+        Adjlist[src].push_back(des);
+    }
+
+    return Adjlist;
+}
+
+int dfs(std::string node, std::string target, std::unordered_map<std::string, std::vector<std::string>>& adjList, std::set<std::string>& visit){
+    if(visit.count(node)){
+        return 0;
+    }
+    
+    if(node == target){
+        return 1; 
+    }
+
+    int count = 0;
+    visit.insert(node);
+    for(auto neighbour : adjList[node]){
+        count += dfs(neighbour, target, adjList, visit);
+    }
+
+    visit.erase(node);
+    return count;
+}
+
+int main(int argc, const char** argv) {
+    std::vector<std::vector<std::string>> edges = 
+            {{"A", "B"}, {"B", "C"}, {"B", "E"}, {"C", "E"}, {"E", "D"}};
+
+    std::unordered_map<std::string, std::vector<std::string>> adjList = buildAdjList(edges);
+
+    /* for(auto edge : adjList){ */
+    /*     std::cout << edge.first << " "; */
+
+    /*     for(auto neighbour : edge.second){ */
+    /*         std::cout << neighbour << " "; */
+    /*     } */
+    /*     std::cout << std::endl; */
+    /* } */
+
+    std::set<std::string> visit;
+    std::cout << dfs("A", "D", adjList, visit) << std::endl;
+    return 0;
+}
